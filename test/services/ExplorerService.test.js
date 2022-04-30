2fef814fd159102147f9a1cd46b1be39c6807a99
Erykzon <jota.once6mail.com>
2022-04-30 18:53:51 -0500

    Probando requerimiento 1 7ta

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,13 +1,14 @@
 const ExplorerService = require('./../../lib/services/ExplorerService')
-const explorers = [{mission: "node"}]
 
 describe ("Test de Exploer Service ", () => {
     test("Requerimiento 1: Filtrar los explrers en la mision  Node ",() =>{
+        const explorers = [{mission: "node"}]
         const explorersInNode = ExplorerService.filterByMission(explorers, "node")
         // expect(explorersInNode.mission).toMatch(/node/)   
         expect(explorersInNode.mission).toMatch(/node/)   
     })
     test("Requerieminto 2: Calcular el total de explorer en la mision Node ", ()=>{
+        const explorers = [{mission: "node"}]
         const Amount = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
         expect(Amount.length).toBe(1)
     })
