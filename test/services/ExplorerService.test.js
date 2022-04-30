782cd9aca801b9a0983fabefe5e6377dfa3962ed
Erykzon <jota.once6mail.com>
2022-04-30 17:04:53 -0500

    Prueba lista de G-A

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -4,7 +4,7 @@ describe ("Test de Exploer Service ", () => {
     test("Requerimiento 1: Calcular los explrers en el proyecto Node ",() =>{
         const explorers = [{mission: "node"}]
         const explorersInNode = ExplorerService.filterByMission(explorers, "node")
-        expect(explorersInNode.length).toBe(2)   
+        expect(explorersInNode.length).toBe(1)   
     })
 
 })
