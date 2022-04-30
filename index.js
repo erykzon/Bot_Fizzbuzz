cb5da840cf3a8ed8d0bf51d3436e3d1772fbf0a4
Erykzon <jota.once6mail.com>
2022-04-30 16:14:10 -0500

    Actualizando index agregando los requerimeintos de explorer services

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Reader = require("./../fizzbuzz/lib/utils/Reader")
 
+const ExplorerService =  require('./../fizzbuzz/lib/services/ExplorerService')
 
 const explorers = Reader.readJsonFile("explorers.json")
 // console.log(explorers)
+console.log(ExplorerService.filterByMission(explorers,"node"))
