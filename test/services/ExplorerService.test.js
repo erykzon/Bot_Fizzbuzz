2c59f2f892268005ca78829eaf190f545f590e42
Erykzon <jota.once6mail.com>
2022-04-30 18:58:41 -0500

    Probando requerimiento 1 8ta

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -5,7 +5,9 @@ describe ("Test de Exploer Service ", () => {
         const explorers = [{mission: "node"}]
         const explorersInNode = ExplorerService.filterByMission(explorers, "node")
         // expect(explorersInNode.mission).toMatch(/node/)   
-        expect(explorersInNode.mission).toMatch(/node/)   
+        expect(explorersInNode.mission).toMatch(explorers.mission)
+        expect(explorersInNode.mission).toBe("node")
+
     })
     test("Requerieminto 2: Calcular el total de explorer en la mision Node ", ()=>{
         const explorers = [{mission: "node"}]
