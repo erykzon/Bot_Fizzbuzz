bb7c8c748a75a7af01d5a8e2736ca5409e654537
Erykzon <jota.once6mail.com>
2022-04-29 22:26:32 -0500

    Agregando 2do  test de Reader.js

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,11 @@
+const fs = require ('fs');
+const Reader = require("./../../lib/utils/Reader")
+
+describe("test for reader ", () => {
+    test("Read json file", () => {
+        const Reader1  = Reader.readJsonFile("explorers.json")
+        const rawdata = fs.readFileSync("explorers.json") 
+        expect(Reader1).toEqual(JSON.parse(rawdata))
+    })
+
+})
