f97f9b6227c4d2da1110d3c3617725988e64c48d
Erykzon <jota.once6mail.com>
2022-05-03 20:10:16 -0500

    Se agrego reader en carpeta test

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,11 +1,11 @@
-const fs = require ('fs');
-const Reader = require("./../../lib/utils/Reader")
+const fs = require ("fs");
+const Reader = require("./../../lib/utils/Reader");
 
 describe("test for reader ", () => {
     test("Read json file", () => {
-        const Reader1  = Reader.readJsonFile("explorers.json")
-        const rawdata = fs.readFileSync("explorers.json") 
-        expect(Reader1).toEqual(JSON.parse(rawdata))
-    })
+        const Reader1  = Reader.readJsonFile("explorers.json");
+        const rawdata = fs.readFileSync("explorers.json"); 
+        expect(Reader1).toEqual(JSON.parse(rawdata));
+    });
 
-})
+});
